38e7ee0a3ce117709ec4fb4af20bbf6aa5ef145a
majd atyyat <majd.atyyat@pwc.com>
2023-05-18 13:43:27 +0300

    added CORS Errors

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ app.use(express.json());
 // Database connection
 dbConnect();
 
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
 // Register endpoint
 app.post('/register', async (request, response) => {
   try {
@@ -104,7 +117,7 @@ app.post("/login", (request, response) => {
 app.get("/auth-endpoint", auth, (request, response) => {
     response.json({ message: "You are authorized to access me" });
   });
-  
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
