2bf1e53b41a9c60656f904f1af30a0c0c857619c
majd atyyat <majd.atyyat@pwc.com>
2023-05-18 16:58:33 +0300

    added line for the deploy

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,12 +1,12 @@
 // external imports
 const mongoose = require("mongoose");
-require('dotenv').config()
+
 
 async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
     .connect(
-        process.env.DB_URL,
+        process.env.MONGODB_CONNECTION_URL,
       {
         //   these are options to ensure that the connection is done properly
         useNewUrlParser: true,
